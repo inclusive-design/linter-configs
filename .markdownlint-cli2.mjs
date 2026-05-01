e039a0e5be6a283d6b729177e3120b6e01b3767d
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-05-01 10:26:25 -0300

    test: move tests into packages (#6)

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -2,5 +2,5 @@ import markdownlintConfig from './packages/markdownlint-config/index.js';
 
 export default {
 	config: markdownlintConfig.config,
-	ignores: ['node_modules', 'test', '**/CHANGELOG.md'],
+	ignores: ['node_modules', 'test', '**/CHANGELOG.md', 'packages/markdownlint-config/test/fixtures/bad/*'],
 };
